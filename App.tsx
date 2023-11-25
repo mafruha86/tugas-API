742ac2cd4224b3fa433ea4535aabf049cbd39cf7
khoirulmuasin <gabutcrypto1@gmail.com>
2023-11-25 18:48:51 +0700

    API

--- App.tsx
+++ App.tsx
@@ -8,7 +8,7 @@ const FlowerShop = () => {
   useEffect(() => {
     const fetchData = async () => {
       try {
-        const response = await axios.get('https://example.com/api/flowers');
+        const response = await axios.get('https://newsapi.org/v2/top-headlines');
         setFlowers(response.data);
       } catch (error) {
         console.error('Error fetching data:', error);
@@ -18,12 +18,28 @@ const FlowerShop = () => {
     fetchData();
   }, []);
 
-  return (
-    <View>
-      <Text>List of Flowers:</Text>
-      {flowers.map((flower) => (
-        <Text key={flower.id}>{flower.name}</Text>
-      ))}
-    </View>
-  );
-};
+      useEffect(() => {
+        getData()
+    }, [])
+
+    return (
+        <>
+            {/*console.log(data)*/}
+            <SafeAreaView>
+                <ScrollView>
+                    {data && data.map((item, i) => {
+                        return <>
+                            <View style={{ flexDirection: 'row', marginVertical: 10, marginHorizontal: 5 }}>
+                                <image style={{ width: 100, height: 100 }} source={{ uri: item.urlToImage }} />
+                                <View style={{ justifyContent: 'space-between' }}>
+                                    <text style={{ fontWeight: 'bold' }}>{item.title}</text>
+                                    <text>{item.author}</text>
+                                </View>
+                            </View>
+                        </>;
+                    })}
+                </ScrollView>
+            </SafeAreaView>
+        </>
+    )
+}
